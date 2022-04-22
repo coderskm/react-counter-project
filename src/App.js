66a29feac6ca8196288d969f71ae3453e74555d4
coderskm <sumitkumarmishra.code@gmail.com>
2022-04-22 11:35:34 +0530

    react counter project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const setIncrement = () => {
+    setCounter(counter + 1);
+  }
+  const setReset = () => {
+    setCounter(0);
+  }
+  const setDecrement = () => {
+    setCounter(counter - 1);
+    if (counter === 0) {
+      setCounter(0);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>
+        <h1 className="text-center font-bold text-5xl m-10">{counter}</h1>
+      </div>
+      <div className="text-center space-x-4 m-10 xsm:space-y-3">
+       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded " onClick={setIncrement}>INCREMENT</button>
+       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " onClick={setReset}>RESET</button>
+       <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded " onClick={setDecrement}>DECREMENT</button>
+      </div>
+    </>
   );
+      
 }
 
 export default App;
